3b7492c538cc9fd47c07e456827436ce4b046df6
jamil.matheny <jamil.matheny@gmail.com>
2025-07-02 19:07:49 +0000

    Conditionally apply basePath and assetPrefix in next.config.ts for GH Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+
 import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/radiology-ai-dashboard' : '', // Apply basePath only in production
+  assetPrefix: isProd ? '/radiology-ai-dashboard/' : '', // Apply assetPrefix only in production
 };
 
 export default nextConfig;
